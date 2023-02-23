82208985ee0383dc324a561533696ddb9eb06213
nghiaddbk <nghiaddbk@outlook.com.vn>
2023-02-23 15:41:57 +0700

    add example to creating user

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,6 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Email',
+    minimum: 5,
+    default: 'nghiadd@example.com',
+  })
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Name',
+    minimum: 5,
+    default: 'nghiadd',
+  })
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Password',
+    minimum: 8,
+    default: '12345678abc',
+  })
   password: string;
 }
 
